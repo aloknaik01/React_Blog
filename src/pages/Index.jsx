f901427c1fb60dc57c74dc0fc18f71136166c869
aloknaik <aloknaik016261@gmail.com>
2025-09-09 19:51:48 +0530

    add hero page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import Hero from "../components/ui/Hero/Hero";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-bg-primary">
+    <div className="min-h-screen bg-bg-primary pt-[110px]">
       <main>
-        
+        <Hero />
       </main>
     </div>
   );
